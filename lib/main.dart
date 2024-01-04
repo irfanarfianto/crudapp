1e4de15de15aad1ed87e21c180837cc46d769240
irfanarfianto <irfanarfianto92@gmail.com>
2024-01-04 10:25:21 +0700

    Update main.dart with theme and title

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:crudapp/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -17,6 +17,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "CRUDAPP",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.grey[200],
+        primaryColor: Colors.deepPurpleAccent,
+        appBarTheme: const AppBarTheme(color: Colors.deepPurple),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurple,
+                foregroundColor: Colors.white)),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.deepPurple,
+            ),
+          ),
+        ),
+      ),
       home: HomeScreen(),
     );
   }
